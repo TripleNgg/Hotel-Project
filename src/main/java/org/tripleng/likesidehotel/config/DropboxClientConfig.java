0e7edca3ece5f0005973057658532f87b778753d
TripleNgg <153419080+TripleNgg@users.noreply.github.com>
2024-03-11 23:12:34 +0700

    add access token

--- src/main/java/org/tripleng/likesidehotel/config/DropboxClientConfig.java
+++ src/main/java/org/tripleng/likesidehotel/config/DropboxClientConfig.java
@@ -14,12 +14,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DropboxClientConfig {
 
-    @Value("${dropbox.key.access-token}")
-    private static String  ACCESS_TOKEN ;
+//    @Value("${dropbox.key.access-token}")
+    private static String  ACCESS_TOKEN  = "sl.BxPzwa-P8lvmDabypnfaeZfsJ-ArVWJc1kVKPKlbUsMHHO1-XV1h1HFk5u7hymnwhAOMdMX4cLs9QM7y-o4jcOFvK8GBWx5xHF5VyZXZYktUnlZEFI5WuVB2BBGIB3dOP_c_75LjIi2za_8y_5pf1Ug";
     @Bean
     public DbxClientV2 clientV2() throws DbxException {
         DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/lakeside-hotel").build();
-
+        System.out.println(ACCESS_TOKEN);
         return new DbxClientV2(config, ACCESS_TOKEN);
     }
 }
