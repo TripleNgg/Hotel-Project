c61de24435ff2890e88ee75b7964ceb09423ea52
TripleNgg <153419080+TripleNgg@users.noreply.github.com>
2024-03-11 22:19:12 +0700

    add dropbox file config

--- src/main/java/org/tripleng/likesidehotel/config/DropboxClientConfig.java
+++ src/main/java/org/tripleng/likesidehotel/config/DropboxClientConfig.java
@@ -7,13 +7,15 @@ import com.dropbox.core.v2.DbxClientV2;
 import com.dropbox.core.v2.files.ListFolderResult;
 import com.dropbox.core.v2.files.Metadata;
 import com.dropbox.core.v2.users.FullAccount;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DropboxClientConfig {
 
-    private static String  ACCESS_TOKEN = "sl.BxNi981cSgOHH9LjaPk2mvAnEnLAMb8RVN_vcjRYNNnbu7ZQuFcvodreklt64KML0WRcwV33_bMFbnpDpsQ7uH167iNG_9GYU7Pv-MWv1ZHYAOgUHIcvV88FlmExewowJ4iIJ5dHXWg4jAno8wQ_dMA";
+    @Value("${dropbox.key.access-token}")
+    private static String  ACCESS_TOKEN ;
     @Bean
     public DbxClientV2 clientV2() throws DbxException {
         DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/lakeside-hotel").build();
